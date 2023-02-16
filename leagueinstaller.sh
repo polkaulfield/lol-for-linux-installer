6d3a501899d5bbfe0b41a1f489bdb527cc490374
kaßin <kassin.dornelles@gmail.com>
2023-02-16 20:25:47 -0300

    [META] Removed cancel button and other fixes
    Removed the Cancel button from the Kdialog module since it does nothing
    Fixed some text and code comments

--- leagueinstaller.sh
+++ leagueinstaller.sh
@@ -11,14 +11,12 @@ function log_message() {
 }
 
 # Show a dialog with an Install and a Cancel button
-kdialog --title "Install LOL?" --yesno "Do you want to install LOL? \n (Before proceeding make sure that you are not running this script as root)"
+kdialog --title "Install LOL?" --yesno "Do you want to install League of Legends? \n (Before proceeding make sure that you are not running this script as root)"
 
-# Check the exit status of the dialog
 if [ $? -eq 0 ]; then
-  # If Install was clicked, create the lol folder in the user's home directory
-  # Create leagueoflegends directory in home directory if it doesn't exist
 
   dbusRef=`kdialog --title "Installing LoL" --progressbar "Initializing" 15`
+  qdbus $dbusRef showCancelButton false
 
 leagueoflegends_dir="${XDG_DATA_HOME}/leagueoflegends"
 if [ ! -d "$leagueoflegends_dir" ]; then
@@ -26,8 +24,8 @@ if [ ! -d "$leagueoflegends_dir" ]; then
   log_message "Created directory $leagueoflegends_dir"
 fi
 
-
 # Create Downloads directory in leagueoflegends directory
+
 downloads_dir="$leagueoflegends_dir/Downloads"
 if [ -d "$downloads_dir" ]; then
   rm -rf "$downloads_dir"
@@ -40,22 +38,23 @@ qdbus $dbusRef Set "" value 1
 qdbus $dbusRef setLabelText "Directory created"
 sleep 1
 
-# Download League installer file and wine translation layer for League
-qdbus $dbusRef setLabelText "Downloading WINE-LOL-LUTRIS and League of Legends installer..."
+# Download League installer file and the wine translation layer for League
+
+qdbus $dbusRef setLabelText "Downloading wine-lol-lutris and the League of Legends installer"
 league_installer_url="https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe"
 wine_lutris_ge_lol_url="https://github.com/GloriousEggroll/wine-ge-custom/releases/download/7.0-GE-5-LoL/wine-lutris-ge-lol-7.0-5-x86_64.tar.xz"
 
-log_message "Downloading files..."
+log_message "Downloading wine-lol-lutris and the League of Legends installer"
 wget -P "$downloads_dir" "$league_installer_url"
 wget -P "$downloads_dir" "$wine_lutris_ge_lol_url"
 log_message "Finished downloading files"
 qdbus $dbusRef Set "" value 2
-qdbus $dbusRef setLabelText "WINE-LOL-LUTRIS and League of Legends installer downloaded."
+qdbus $dbusRef setLabelText "wine-lol-lutris and League of Legends installer downloaded"
 sleep 1
 
 # Extract wine translation layer file to wine directory
-qdbus $dbusRef Set "" value 3
-qdbus $dbusRef setLabelText "Extracting WINE-LUTRIS-LOL..."
+
+qdbus $dbusRef setLabelText "Extracting wine-lutris-lol package, please wait"
 
 wine_dir="$leagueoflegends_dir/wine"
 if [ -d "$wine_dir" ]; then
@@ -65,7 +64,10 @@ fi
 mkdir "$wine_dir"
 log_message "Created directory $wine_dir"
 tar -xf "$downloads_dir/wine-lutris-ge-lol-7.0-5-x86_64.tar.xz" -C "$wine_dir" --strip-components=1
-qdbus $dbusRef setLabelText "Extracted WINE-LUTRIS-LOL."
+
+qdbus $dbusRef Set "" value 3
+qdbus $dbusRef setLabelText "Extracted wine-lutris-lol"
+
 
 # Rename League installer file to leagueinstaller.exe
 qdbus $dbusRef setLabelText "Renaming installer file"
@@ -84,11 +86,11 @@ for subfolder in "$wine_dir"/*; do
   fi
 done
 qdbus $dbusRef Set "" value 4
-qdbus $dbusRef setLabelText "Renamed lutris-lol folder."
+qdbus $dbusRef setLabelText "Renamed lutris-lol folder"
 sleep 1
 
 # Create prefix directory inside wine directory
-qdbus $dbusRef setLabelText "Creating WINE prefix folder."
+qdbus $dbusRef setLabelText "Creating WINE prefix folder"
 
 prefix_dir="$wine_dir/prefix"
 if [ ! -d "$prefix_dir" ]; then
@@ -97,12 +99,12 @@ if [ ! -d "$prefix_dir" ]; then
 fi
 
 qdbus $dbusRef Set "" value 5
-qdbus $dbusRef setLabelText "WINE prefix folder created."
+qdbus $dbusRef setLabelText "WINE prefix folder created"
 sleep 1
 
 # Create and run first-boot script
 log_message "Creating first-boot script"
-qdbus $dbusRef setLabelText "Creating first-boot script."
+qdbus $dbusRef setLabelText "Creating first-boot script"
 
 cd "$leagueoflegends_dir"
 leaguefirstboot="Firstboot.sh"
@@ -129,7 +131,7 @@ chmod +x $leaguefirstboot
 wait
 
 qdbus $dbusRef Set "" value 6
-qdbus $dbusRef setLabelText "First-boot script created."
+qdbus $dbusRef setLabelText "First-boot script created"
 sleep 1
 
 # Launching first-boot script
@@ -167,12 +169,12 @@ chmod +x $leaguelauncherfile
 
 log_message "Launch.sh file created in $leagueoflegends_dir"
 qdbus $dbusRef Set "" value 7
-qdbus $dbusRef setLabelText "Launch.sh file created."
+qdbus $dbusRef setLabelText "Launch.sh file created"
 sleep 1
 
 # Create system menu shortcut
-qdbus $dbusRef setLabelText "Creating system menu shortcut for the Launch.sh script."
-log_message "Creating system menu shortcut for the Launch.sh script."
+qdbus $dbusRef setLabelText "Creating system menu shortcut for the Launch.sh script"
+log_message "Creating system menu shortcut for the Launch.sh script"
 
 cd ~/.local/share/applications
 touch "LeagueofLegendsLauncher.desktop"
@@ -189,11 +191,11 @@ Categories=Game;' > LeagueofLegendsLauncher.desktop
 
 log_message "System menu shortcut created"
 qdbus $dbusRef Set "" value 8
-qdbus $dbusRef setLabelText "System menu shortcut created."
+qdbus $dbusRef setLabelText "System menu shortcut created"
 sleep 1
 
 # Move the .png file to the icons path
-qdbus $dbusRef setLabelText "Installing system icons."
+qdbus $dbusRef setLabelText "Installing system icons"
 
 # Download the image files to the league of legends data directory
 wget -P "$leagueoflegends_dir" https://github.com/kassindornelles/lol-for-linux-bash-installer/raw/main/icons/league16.png
@@ -212,9 +214,9 @@ done
 
 rm "$leagueoflegends_dir/league"*.png
 
-log_message "System icons installed."
+log_message "System icons installed"
 qdbus $dbusRef Set "" value 10
-qdbus $dbusRef setLabelText "System icons are installed."
+qdbus $dbusRef setLabelText "System icons are installed"
 sleep 1
 qdbus $dbusRef close
 
