2cd8ce2cc95945acbe6ade2560d9919c4d4b8de9
kaßin <kassin.dornelles@gmail.com>
2023-02-19 17:44:05 -0300

    [META] Update launch-league.py
    It will actually work now

--- launch-league.py
+++ launch-league.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import os
+import sys
+import subprocess
 
-# XDG stuff
-XDG_DATA_HOME = os.environ.get('XDG_DATA_HOME', os.path.expanduser('~/.local/share'))
+home_dir = os.path.expanduser("~")
+game_main_dir = os.path.join(home_dir, 'leagueoflegends')
 
 # Step 1: Check if leagueoflegends directory and Launch.sh script exist
-if os.path.isdir(f"{XDG_DATA_HOME}/leagueoflegends") and os.path.isfile(f"{XDG_DATA_HOME}/leagueoflegends/Launch.sh"):
+if os.path.isdir(f"{game_main_dir}") and os.path.isfile(f"{game_main_dir}/launch-league-of-legends.py"):
     # Step 2: Start the Launch.sh script
     print("League of legends was detected, running the game...")
-    os.chdir(f"{XDG_DATA_HOME}/leagueoflegends")
-    os.chmod("./Launch.sh", 0o777)
-    os.system("./Launch.sh")
+    os.chdir(f"{game_main_dir}")
+    os.chmod("launch-league-of-legends.py", 0o777)
+    os.system("python launch-league-of-legends.py")
 else:
     # Step 3: Launch leagueinstaller.py from current directory
     print("League of legends install not detected, installing the game...")
-    os.chmod("python ./leagueinstaller.py", 0o777)
-    os.system("python ./leagueinstaller.py")
+    os.chmod("leagueinstaller.py", 0o777)
+    os.system("python leagueinstaller.py")
