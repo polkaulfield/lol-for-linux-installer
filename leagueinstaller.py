b2405e17e2fcddf5ce54a5e37fcc4772f267ab46
kaßin <kassin.dornelles@gmail.com>
2023-03-21 02:13:18 -0300

    [Python] Permisions to uninstall.py file
    and more

--- leagueinstaller.py
+++ leagueinstaller.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 import os, sys, shutil, requests, tarfile, lzma, subprocess, signal, json
 from PyQt5.QtCore import pyqtSignal
 
+
 def league_install_code(game_main_dir):
     # Expose variables
-    print("Setting all variables") # Cheap logging
+    print("Setting all variables")  # Cheap logging
     home_dir = os.path.expanduser("~")
     game_downloads_dir = os.path.join(game_main_dir, 'downloads')
     game_winetricks_cache_dir = os.path.join(game_downloads_dir, "winetricks-cache")
@@ -14,19 +16,24 @@ def league_install_code(game_main_dir):
     user_icons_folder = os.path.join(home_dir, user_local_share, "icons")
     user_hicolor_folder = os.path.join(user_icons_folder, "hicolor")
     user_applications_folder = os.path.join(home_dir, user_local_share, "applications")
-    desktop_file_path = os.path.join(os.path.expanduser("~"), ".local", "share", "applications", "LeagueLauncherPython.desktop")
+    desktop_file_path = os.path.join(os.path.expanduser("~"), ".local", "share", "applications",
+                                     "LeagueLauncherPython.desktop")
     game_launch_file_path = os.path.join(game_main_dir, "launch-league-of-legends.py")
 
     # Create all folders that we are going to use
-    folder_paths = [game_main_dir, game_downloads_dir, game_main_wine_dir, game_prefix_dir, game_winetricks_cache_dir, user_icons_folder, user_hicolor_folder, os.path.join(user_hicolor_folder, "16x16"), os.path.join(user_hicolor_folder, "32x32"), os.path.join(user_hicolor_folder, "48x48"), os.path.join(user_hicolor_folder, "64x64"), os.path.join(user_hicolor_folder, "128x128"), os.path.join(user_hicolor_folder, "256x256"), user_applications_folder]
-    print("Creating folders for our League install") # Cheap logging
+    folder_paths = [game_main_dir, game_downloads_dir, game_main_wine_dir, game_prefix_dir, game_winetricks_cache_dir,
+                    user_icons_folder, user_hicolor_folder, os.path.join(user_hicolor_folder, "16x16"),
+                    os.path.join(user_hicolor_folder, "32x32"), os.path.join(user_hicolor_folder, "48x48"),
+                    os.path.join(user_hicolor_folder, "64x64"), os.path.join(user_hicolor_folder, "128x128"),
+                    os.path.join(user_hicolor_folder, "256x256"), user_applications_folder]
+    print("Creating folders for our League install")  # Cheap logging
     for folder_path in folder_paths:
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
             os.chmod(folder_path, 0o700)
 
     # Download necessary files
-    print("Downloading wine-lutris-lol build") # Cheap logging
+    print("Downloading wine-lutris-lol build")  # Cheap logging
     wine_lutris_build_url = "https://github.com/GloriousEggroll/wine-ge-custom/releases/download/7.0-GE-5-LoL/wine-lutris-ge-lol-7.0-5-x86_64.tar.xz"
     tar_file_name = "wine-lutris-ge-lol-7.0-5-x86_64.tar.xz"
     wine_lutris_build_file = os.path.join(game_downloads_dir, tar_file_name)
@@ -34,7 +41,7 @@ def league_install_code(game_main_dir):
     with open(wine_lutris_build_file, "wb") as f:
         f.write(response.content)
 
-    print("Downloading League of Legends installer from Riot NA Servers") # Cheap logging
+    print("Downloading League of Legends installer from Riot NA Servers")  # Cheap logging
     exe_file_name = "live.na.exe"
     league_installer_url = "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe"
     league_installer_file = os.path.join(game_downloads_dir, exe_file_name)
@@ -42,25 +49,25 @@ def league_install_code(game_main_dir):
     with open(league_installer_file, "wb") as f:
         f.write(response.content)
 
-    print("All files Downloaded") # Cheap logging
+    print("All files Downloaded")  # Cheap logging
 
     # Extract tar file
-    print("Extracting the wine-lutris-lol build file") # Cheap logging
+    print("Extracting the wine-lutris-lol build file")  # Cheap logging
     with tarfile.open(os.path.join(game_downloads_dir, tar_file_name)) as file:
         file.extractall(os.path.join(game_main_wine_dir))
-    print("Extraction on the wine-lutris-lol build file completed") # Cheap logging
+    print("Extraction on the wine-lutris-lol build file completed")  # Cheap logging
 
     # Start the first-boot script to setup DXVK and the prefix
-    first_boot_envs = { **os.environ,
-            "PATH": f"{game_main_wine_dir}/lutris-ge-lol-7.0-5-x86_64/bin:{os.environ['PATH']}",
-            "WINEARCH": "win64",
-            "WINEPREFIX": game_prefix_dir,
-            "WINELOADER": f"{game_main_wine_dir}/lutris-ge-lol-7.0-5-x86_64/bin/wine",
-            "WINEFSYNC": "1",
-            "WINEDEBUG": "-all",
-            "WINEDLLOVERRIDES": "winemenubuilder.exe=d",
-            "WINETRICKS_CACHE": f"{game_winetricks_cache_dir}",
-        }
+    first_boot_envs = {**os.environ,
+                       "PATH": f"{game_main_wine_dir}/lutris-ge-lol-7.0-5-x86_64/bin:{os.environ['PATH']}",
+                       "WINEARCH": "win64",
+                       "WINEPREFIX": game_prefix_dir,
+                       "WINELOADER": f"{game_main_wine_dir}/lutris-ge-lol-7.0-5-x86_64/bin/wine",
+                       "WINEFSYNC": "1",
+                       "WINEDEBUG": "-all",
+                       "WINEDLLOVERRIDES": "winemenubuilder.exe=d",
+                       "WINETRICKS_CACHE": f"{game_winetricks_cache_dir}",
+                       }
 
     subprocess.run(["winetricks", "dxvk"], env=first_boot_envs, check=True)
     wine_process = ["wine", league_installer_file]
@@ -84,7 +91,8 @@ def league_install_code(game_main_dir):
         file.write('        WINEDEBUG="-all",\n')
         file.write('        WINEDLLOVERRIDES="winemenubuilder.exe=d",\n')
         file.write('    )\n')
-        file.write('wine_process = ["wine", os.path.join(game_exe_path, game_exe_file_name), "--launch-product=league_of_legends", "--launch-patchline=live"]\n')
+        file.write(
+            'wine_process = ["wine", os.path.join(game_exe_path, game_exe_file_name), "--launch-product=league_of_legends", "--launch-patchline=live"]\n')
         file.write('subprocess.run(wine_process, env=start_game_vars, check=True)\n')
 
     # Create .desktop file
@@ -154,4 +162,5 @@ def league_install_code(game_main_dir):
 
     # Copy uninstaller
     shutil.copy("uninstall.py", os.path.join(game_main_dir, "uninstall.py"))
+    os.chmod(os.path.join(game_main_dir), "uninstall.py", 0o777)
     print("Created uninstall.py file in game dir")
