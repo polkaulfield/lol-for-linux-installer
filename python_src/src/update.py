7e106de548a92575eee0d0627128c7ed04442332
kaßin <kassin.dornelles@gmail.com>
2023-04-14 18:08:06 -0300

    [Python] Simple wine-build update system

--- python_src/src/update.py
+++ python_src/src/update.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import json, urllib.request, os, re
+import json, sys, tarfile, shutil, os, urllib.request
 
 url = "https://raw.githubusercontent.com/kassindornelles/lol-for-linux-installer/main/wine_build.json"
 
@@ -12,15 +12,38 @@ with open(filename, "r") as f:
 with open("buildversion.json", "r") as f:
     existing_data = json.load(f)
 
-current_version = re.search(r"(\d+\.\d+)-GE-(\d+)-LoL/wine-lutris-ge-lol-(\d+\.\d+\.\d+)-x86_64.tar.xz", data["current_build_name"]).group(3)
-existing_version = re.search(r"(\d+\.\d+)-GE-(\d+)-LoL/wine-lutris-ge-lol-(\d+\.\d+\.\d+)-x86_64.tar.xz", existing_data["current_build_name"]).group(3)
+print("Value of wine_build.json:", data["current_build_name"])
+print("Value of buildversion.json:", existing_data["current_build_name"])
 
-print("Value of wine_build.json:", current_version)
-print("Value of buildversion.json:", existing_version)
-
-if current_version <= existing_version:
-    print("No need to update")
-else:
+if data["current_build_name"].split("/")[-1] > existing_data["current_build_name"].split("/")[-1]:
     print("Update needed")
 
+    build_url = data["current_build_name"]
+    build_filename = build_url.split("/")[-1]
+    urllib.request.urlretrieve(build_url, build_filename)
+
+    temp_dir = "tmp"
+    os.makedirs(temp_dir, exist_ok=True)
+    with tarfile.open(build_filename, "r:xz") as tar:
+        tar.extractall(path=temp_dir)
+
+    wine_build_dir = "wine/wine-build"
+    if os.path.exists(wine_build_dir):
+        shutil.rmtree(wine_build_dir)
+
+    extract_path = os.path.join(temp_dir, os.listdir(temp_dir)[0])
+    shutil.move(extract_path, wine_build_dir)
+
+    existing_data["current_build_name"] = data["current_build_name"]
+    with open("buildversion.json", "w") as f:
+        json.dump(existing_data, f)
+
+else:
+    print("No need to update")
+    sys.exit(0)
+
 os.remove(filename)
+if os.path.exists(build_filename):
+    os.remove(build_filename)
+if os.path.exists(temp_dir):
+    shutil.rmtree(temp_dir)
