2496c71ed4fb05669b8504d061de541e5b4918db
Kassin Dornelles <kassin.dornelles@gmail.com>
2023-05-24 14:47:25 -0300

    Update ubuntu-package.sh

--- ubuntu-package.sh
+++ ubuntu-package.sh
@@ -9,7 +9,7 @@ url="https://github.com/kassindornelles/lol-for-linux-installer"
 license="GPL-3.0"
 arch="amd64"
 
-dependencies=$(dpkg-query -W -f='${Depends}' python3 python3-psutil python3-pyqt5 wine64 python3-requests qt5-default libgnutls30 libldap-2.4-2 libpng16-16 libgphoto2-6 libpulse0 libqt5gui5 | sed 's/,/ /g')
+dependencies="python3, python3-psutil, python3-pyqt5, wine-stable, python3-requests, qtbase5-dev, libgnutls30, libldap-dev, libpng16-16, libgphoto2-6, libpulse0, libqt5gui5, libc6"
 
 pkgdir="$pkgname-$pkgver"
 mkdir -p "$pkgdir/DEBIAN"
