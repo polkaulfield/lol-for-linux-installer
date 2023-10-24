a978f03de34b75b1b2008a6269e07a8418c1f9a8
Kassin Dornelles <kassin.dornelles@gmail.com>
2023-10-24 19:36:05 -0300

    2.6.3
    Skipping one tag.

--- src/lolforlinuxinstaller.py
+++ src/lolforlinuxinstaller.py
@@ -55,7 +55,7 @@ class Installer(QMainWindow):
             loadUi("installer.ui", self)
         except:
             loadUi("/usr/share/lol-for-linux-installer/installer.ui", self)
-        self.currentversion = "2.6.1"
+        self.currentversion = "2.6.3"
         self.slider_value_changed = False
         self.game_installed_folder = None
         self.game_rpc_folder = None
